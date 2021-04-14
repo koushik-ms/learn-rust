bc412dc1da9b53812f72cd493f661e9ac1cb20aa
koushik-ms <koushik.ms@gmail.com>
2021-04-14 18:01:26 +0000

    typical leap and common years

--- leap/src/lib.rs
+++ leap/src/lib.rs
@@ -1,5 +1,5 @@
 fn is_leap_year(year: u32) -> bool {
-    true
+    year%4 == 0
 }
 #[cfg(test)]
 mod tests {
@@ -11,5 +11,9 @@ mod tests {
     #[test]
     fn y1990_is_not_a_leap_year() {
         assert_eq!(is_leap_year(1990), false);
+    }
+    #[test]
+    fn y1996_is_a_leap_year() {
+        assert_eq!(is_leap_year(1996), true);
     }
 }
