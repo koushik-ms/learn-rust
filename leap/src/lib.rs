ff8545e44ee6bf3fc01e16b1bd815e1b8579ebc1
koushik-ms <koushik.ms@gmail.com>
2021-04-14 18:04:20 +0000

    atypical common/ leap years

--- leap/src/lib.rs
+++ leap/src/lib.rs
@@ -1,13 +1,9 @@
 fn is_leap_year(year: u32) -> bool {
-    year%4 == 0
+    year%4 == 0 && (year%100 != 0 || year%400 == 0)
 }
 #[cfg(test)]
 mod tests {
     use super::*;
-    #[test]
-    fn can_call_is_leap() {
-        assert_eq!(is_leap_year(2000), true);
-    }
     #[test]
     fn y1990_is_not_a_leap_year() {
         assert_eq!(is_leap_year(1990), false);
@@ -15,5 +11,13 @@ mod tests {
     #[test]
     fn y1996_is_a_leap_year() {
         assert_eq!(is_leap_year(1996), true);
+    }
+    #[test]
+    fn y1900_is_atypical_common_year() {
+        assert_eq!(is_leap_year(1900), false);
+    }
+    #[test]
+    fn y2000_is_atypical_leap_year() {
+        assert_eq!(is_leap_year(2000), true);
     }
 }
