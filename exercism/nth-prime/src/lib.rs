332f01670b02981b0fc38a30ec4edbcdf7f70f74
koushik-ms <koushik.ms@gmail.com>
2021-05-19 08:01:55 +0000

    make iter idiomatic

--- exercism/nth-prime/src/lib.rs
+++ exercism/nth-prime/src/lib.rs
@@ -7,20 +7,18 @@ fn is_prime(p: u32) -> bool {
     true
 }
 
-fn nxt_prime(x: &mut u32) {
-    let mut p = *x+1;
+fn nxt_prime(x: &mut u32) -> u32 {
+    *x += 1;
     loop {
-        if is_prime(p) { break; }
-        p += 1;
+        if is_prime(*x) { break; }
+        *x += 1;
     }
-    *x=p
-}
-pub fn nth(n: u32) -> u32 {
-    let mut ans: u32 = 1;
-    let mut ps = std::iter::from_fn(|| {
-        nxt_prime(&mut ans);
-        Some(ans)
-    });
-    ps.nth(n as usize).unwrap()
+    *x
 }
 
+pub fn nth(n: u32) -> u32 {
+    let mut ans: u32 = 1;
+    std::iter::from_fn(|| {
+        Some(nxt_prime(&mut ans))
+    }).nth(n as usize).unwrap()
+}
