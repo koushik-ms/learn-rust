0ccee56261ba73e8f8fa982cf975845da50cf28a
koushik-ms <koushik.ms@gmail.com>
2021-05-19 08:24:48 +0000

    idiomatic, functional, small and fast

--- exercism/nth-prime/src/lib.rs
+++ exercism/nth-prime/src/lib.rs
@@ -1,22 +1,10 @@
 
-fn is_prime(p: u32) -> bool {
-    if p < 3 {return true;}
-    let l = ((p as f64).sqrt()) as u32;
-    for i in 2..=l {
-        if p%i == 0 {return false;}
+fn is_prime(p:& u32) -> bool {
+    (*p < 3) || {
+        let l = ((*p as f64).sqrt()) as u32;
+        !(2..=l).any(|i| (*p)%i == 0)
     }
-    true
 }
-
-fn nxt_prime(x: &mut u32) -> u32 {
-    *x += 1;
-    while !is_prime(*x) { *x += 1; }
-    *x
-}
-
 pub fn nth(n: u32) -> u32 {
-    let mut ans: u32 = 1;
-    std::iter::from_fn(|| {
-        Some(nxt_prime(&mut ans))
-    }).nth(n as usize).unwrap()
+    (2u32..).filter(is_prime).nth(n as usize).unwrap()
 }
