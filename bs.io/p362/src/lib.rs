54c35d683c1cc6942a57d62267c928fcbaceea77
koushik-ms <koushik.ms@gmail.com>
2021-05-20 10:19:18 +0000

    it works!

--- bs.io/p362/src/lib.rs
+++ bs.io/p362/src/lib.rs
@@ -1,7 +1,10 @@
+fn solve(s: &str) -> bool { true }
+
 #[cfg(test)]
 mod tests {
+    use super::solve;
     #[test]
     fn it_works() {
-        assert_eq!(2 + 2, 4);
+        assert_eq!(solve("faf"), true);
     }
 }
