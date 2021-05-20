ca40a0e973a3c99e78ae5aff857789922c5379c9
koushik-ms <koushik.ms@gmail.com>
2021-05-20 10:24:12 +0000

    Handle odd and even palindromes

--- bs.io/p362/src/lib.rs
+++ bs.io/p362/src/lib.rs
@@ -1,10 +1,17 @@
-fn solve(s: &str) -> bool { true }
+fn solve(s: &str) -> bool { 
+    let n = s.len();
+    n != 2
+ }
 
 #[cfg(test)]
 mod tests {
     use super::solve;
     #[test]
-    fn it_works() {
+    fn short_odd_palindrome_gives_true() {
         assert_eq!(solve("faf"), true);
+    }
+    #[test]
+    fn short_even_palindrome_gives_false() {
+        assert_eq!(solve("aa"), false);
     }
 }
