4eabcbed8136efe2b43ff0341c53d26366e983db
koushik-ms <koushik.ms@gmail.com>
2021-05-22 19:59:30 +0000

    clean-up implementation

--- exercism/proverb/src/lib.rs
+++ exercism/proverb/src/lib.rs
@@ -6,10 +6,9 @@ pub fn build_proverb(list: &[&str]) -> String {
 }
 
 fn bp(list: &[&str]) -> String {
-    let ans = (1..list.len()).map( |i| {
-        format!("For want of a {} the {} was lost.\n",list[i-1], list[i])
-    }).collect::<Vec<_>>().join(
-        ""
-    );
+    let ans = (1..list.len())
+        .map( |i| format!("For want of a {} the {} was lost.\n",list[i-1], list[i]))
+        .collect::<Vec<_>>()
+        .concat();
     format!("{}And all for the want of a {}.", ans, list[0])
 }
