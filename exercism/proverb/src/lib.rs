ba4be9f421bf806eaa965532cd46e6b10ae179fc
koushik-ms <koushik.ms@gmail.com>
2021-05-22 19:54:31 +0000

    add proverb

---
+++ exercism/proverb/src/lib.rs
@@ -0,0 +1,15 @@
+pub fn build_proverb(list: &[&str]) -> String {
+    match list.len() {
+        0 => "".to_string(),
+        _ => bp(list)
+    }
+}
+
+fn bp(list: &[&str]) -> String {
+    let ans = (1..list.len()).map( |i| {
+        format!("For want of a {} the {} was lost.\n",list[i-1], list[i])
+    }).collect::<Vec<_>>().join(
+        ""
+    );
+    format!("{}And all for the want of a {}.", ans, list[0])
+}
