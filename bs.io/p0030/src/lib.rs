255e43e0876628f80720d1a072a196606ca5175e
koushik-ms <koushik.ms@gmail.com>
2021-05-24 15:54:38 +0000

    one-liner ftw!

--- bs.io/p0030/src/lib.rs
+++ bs.io/p0030/src/lib.rs
@@ -1,4 +1,6 @@
-fn solve(s0: &str, s1: &str) -> bool { true }
+fn solve(s0: &str, s1: &str) -> bool {
+    (s0.to_owned() + s0).contains(s1)
+}
 
 #[cfg(test)]
 mod tests {
@@ -8,7 +10,15 @@ mod tests {
         assert_eq!(2 + 2, 4);
     }
     #[test]
-    fn can_solve_rotation() {
+    fn hello_is_rotation_of_itself() {
         assert!(solve("hello", "hello"));
+    }
+    #[test]
+    fn llohe_is_rotation_of_hello() {
+        assert!(solve("llohe", "hello"));
+    }
+    #[test]
+    fn tap_is_no_rotation_of_pat() {
+        assert!(!solve("Pat", "taP"));
     }
 }
