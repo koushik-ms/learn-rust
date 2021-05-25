b6eb446af9e7e0b2eafb3f8aaf02d9506b6fafb1
koushik-ms <koushik.ms@gmail.com>
2021-05-25 11:01:40 +0000

    Recursive solution passing tests

--- bs.io/p0009/src/lib.rs
+++ bs.io/p0009/src/lib.rs
@@ -1,4 +1,15 @@
-fn solve(n: &Vec<i32>) -> i32 { 13 }
+use std::cmp::max;
+
+fn solve(n: &[i32]) -> i32 { 
+    match n.len() {
+        0 => 0,
+        1 => max(0, n[0]),
+        _ => max(
+            0,
+            max(n[0]+solve(&n[2..]), solve(&n[1..]))
+        ),
+    }
+}
 
 #[cfg(test)]
 mod tests {
@@ -7,5 +18,20 @@ mod tests {
     fn largest_non_adj_sum_positive_numbers() {
         let nums = vec![3, 4, 6, 9, 1];
         assert_eq!(solve(&nums), 13);
+    }
+    #[test]
+    fn largest_non_adj_sum_positive_numbers_skip_two() {
+        let nums = vec![6, 0, 2, 5];
+        assert_eq!(solve(&nums), 11);
+    }
+    #[test]
+    fn largest_non_adj_sum_negative_numbers() {
+        let nums = vec![-3, -4, -6, -9, -1];
+        assert_eq!(solve(&nums), 0);
+    }
+    #[test]
+    fn largest_non_adj_sum_positive_and_negative_numbers() {
+        let nums = vec![3, 4, 6, -9, 1];
+        assert_eq!(solve(&nums), 10);
     }
 }
