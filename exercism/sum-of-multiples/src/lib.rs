8a5c696e24ca0482bb5a2a44c9ab2816992e2a98
koushik-ms <koushik.ms@gmail.com>
2021-05-31 08:05:46 +0000

    make the change easy

--- exercism/sum-of-multiples/src/lib.rs
+++ exercism/sum-of-multiples/src/lib.rs
@@ -1,3 +1,9 @@
+use std::convert::TryInto;
+
+fn is_multiple_of_any(n: &u32, factors: &[u32]) -> bool { false }
+
 pub fn sum_of_multiples(limit: u32, factors: &[u32]) -> u32 {
-    0
+    ((1..=limit)
+        .filter(|x| is_multiple_of_any(x, factors))
+        .count() as usize).try_into().unwrap()
 }
