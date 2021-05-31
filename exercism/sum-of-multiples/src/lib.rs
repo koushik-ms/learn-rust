2bf0294cb560acba4d0f2e3ea20ca86f1fbfd662
koushik-ms <koushik.ms@gmail.com>
2021-05-31 08:25:00 +0000

    Do not consider limit itself

--- exercism/sum-of-multiples/src/lib.rs
+++ exercism/sum-of-multiples/src/lib.rs
@@ -1,11 +1,9 @@
-use std::convert::TryInto;
-
 fn is_multiple_of_any(n: &u32, factors: &[u32]) -> bool { 
     factors.iter().any(|&f| n%f == 0)
  }
 
 pub fn sum_of_multiples(limit: u32, factors: &[u32]) -> u32 {
-    (1..=limit)
+    (1..limit)
         .filter(|x| is_multiple_of_any(x, factors))
         .sum()
 }
