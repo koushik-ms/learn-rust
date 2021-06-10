c3121cad74af20f2992254313448d7cbb840cb1e
koushik-ms <koushik.ms@gmail.com>
2021-06-10 19:18:51 +0000

    Implement custom trait

--- exercism/sum-of-multiples/src/lib.rs
+++ exercism/sum-of-multiples/src/lib.rs
@@ -1,12 +1,19 @@
-fn is_multiple_of_any(n: &u32, factors: &[u32]) -> bool { 
-    factors
-        .iter()
-        .filter(|&&z| z != 0)
-        .any(|&f| n%f == 0)
- }
+trait Multiple {
+    fn is_multiple_of_any(&self, factors: &[u32]) -> bool;
+}
+
+impl Multiple for u32 {
+    fn is_multiple_of_any(&self, factors: &[u32]) -> bool { 
+        factors
+            .iter()
+            .filter(|&&z| z != 0)
+            .any(|&f| self%f == 0)
+    }
+}
+
 
 pub fn sum_of_multiples(limit: u32, factors: &[u32]) -> u32 {
     (1..limit)
-        .filter(|x| is_multiple_of_any(x, factors))
+        .filter(|x| x.is_multiple_of_any(factors))
         .sum()
 }
