db00b35c47cb49579c89aeea1dcc62db50c95669
koushik-ms <koushik.ms@gmail.com>
2021-06-15 11:35:37 +0000

    scaffold tests and solution

--- bs.io/p0010/src/lib.rs
+++ bs.io/p0010/src/lib.rs
@@ -1,7 +1,10 @@
+#[allow(dead_code)]
+fn solve(n: u32) -> u32 { n+1 }
 #[cfg(test)]
 mod tests {
+    use super::solve;
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn climb_4_steps_in_5_ways() {
+        assert_eq!(solve(4), 5);
     }
 }
