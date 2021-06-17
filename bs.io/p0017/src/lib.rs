64b8f80c71b7c8225de894508248c1a78769774e
koushik-ms <koushik.ms@gmail.com>
2021-06-17 17:49:04 +0000

    can (pass) test

--- bs.io/p0017/src/lib.rs
+++ bs.io/p0017/src/lib.rs
@@ -1,7 +1,20 @@
+use std::u32;
+
+#[allow(dead_code)]
+fn traverse(mat: &Vec<Vec<u8>>) -> u32 { 2 }
 #[cfg(test)]
 mod tests {
+    use super::traverse;
+    fn maze(rep: &str) -> Vec<Vec<u8>> {
+        let rows = rep.split(',');
+        rows.map(|r| {
+            r.bytes().map(|c| c - '0' as u8).collect::<Vec<_>>()
+        }).collect()
+    }
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn traverse_001_001_100_in_2_ways() {
+        let m = maze("001,001,100");
+        println!("{:?}", m);
+        assert_eq!(traverse(&m), 2);
     }
 }
