ab757a03c69a2a9c2fbb1db213b90976a508976c
koushik-ms <koushik.ms@gmail.com>
2021-06-17 18:17:29 +0000

    Solve using dynamic programming

--- bs.io/p0017/src/lib.rs
+++ bs.io/p0017/src/lib.rs
@@ -1,7 +1,28 @@
 use std::u32;
 
+const BOUND: u32 = 1_000_000_000 + 7;
+
 #[allow(dead_code)]
-fn traverse(mat: &Vec<Vec<u8>>) -> u32 { 2 }
+fn traverse(mat: &Vec<Vec<u8>>) -> u32 { 
+    let n = mat.len();
+    let m = mat.first().unwrap().len();
+    let mut ways = vec![vec![0;m];n];
+    for i in 0..n {
+        for j in 0..m {
+            if mat[i][j] == 1 { continue; }
+            if i==0 && j==0 {
+                ways[i][j] = 1;
+            } else {
+                ways[i][j] = {
+                    let fu = if 0==i {0} else {ways[i-1][j]};
+                    let fl = if 0==j {0} else {ways[i][j-1]};
+                    (fu+fl)%BOUND
+                };
+            }
+        }
+    }
+    ways[n-1][m-1]
+ }
 #[cfg(test)]
 mod tests {
     use super::traverse;
