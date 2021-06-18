c6ab859f8102a96844c77e6b6367ccb7e05d8286
koushik-ms <koushik.ms@gmail.com>
2021-06-18 17:29:31 +0000

    refactor solution with better names for funs

--- bs.io/p0017/src/lib.rs
+++ bs.io/p0017/src/lib.rs
@@ -1,35 +1,46 @@
-use std::u32;
+use std::{num::NonZeroUsize, u32};
 
 const BOUND: u32 = 1_000_000_000 + 7;
 
 #[allow(dead_code)]
-fn traverse(mat: &Vec<Vec<u8>>) -> u32 { 
+fn traverse(mat: &Vec<Vec<u8>>) -> u32 {
     let n = mat.len();
     let m = mat.first().unwrap().len();
-    let mut ways = vec![vec![0;m];n];
+    let mut ways = vec![vec![0; m]; n];
     for i in 0..n {
         for j in 0..m {
-            if mat[i][j] == 1 { continue; }
-            if i==0 && j==0 {
-                ways[i][j] = 1;
+            if mat[i][j] == 1 {
+                continue;
+            }
+            ways[i][j] = if top_left(i, j) {
+                1
             } else {
-                ways[i][j] = {
-                    let fu = if 0==i {0} else {ways[i-1][j]};
-                    let fl = if 0==j {0} else {ways[i][j-1]};
-                    (fu+fl)%BOUND
-                };
+                bounded_ways_from_neighbours(&ways, i, j)
             }
         }
     }
-    ways[n-1][m-1]
- }
+    ways[n - 1][m - 1]
+}
+
+fn bounded_ways_from_neighbours(ways: &Vec<Vec<u32>>, i: usize, j: usize) -> u32 {
+    (
+        NonZeroUsize::new(i).map_or(0, |_| ways[i - 1][j])
+        + NonZeroUsize::new(j).map_or(0, |_| ways[i][j - 1])
+    ) % BOUND
+}
+
+fn top_left(i: usize, j: usize) -> bool {
+    i == 0 && j == 0
+}
 #[cfg(test)]
 mod tests {
     use super::traverse;
     fn maze(rep: &str) -> Vec<Vec<u8>> {
         let rows = rep.split(',');
         rows.map(|r| {
-            r.bytes().map(|c| c - '0' as u8).collect::<Vec<_>>()
+            r.bytes()
+                .map(|c| c - '0' as u8)
+                .collect::<Vec<_>>()
         }).collect()
     }
     #[test]
