a27cd1bee81979430bfbd5c893fe24ad7a5c1991
koushik-ms <koushik.ms@gmail.com>
2021-06-22 16:27:05 +0000

    remove extra vars and code

--- exercism/prime-factors/src/lib.rs
+++ exercism/prime-factors/src/lib.rs
@@ -1,11 +1,10 @@
-pub fn factors(n: u64) -> Vec<u64> {
+pub fn factors(mut n: u64) -> Vec<u64> {
     let mut ans = Vec::new();
-    let mut m = n;
     for i in 2.. {
-        if i > m { break; }
-        while m%i == 0 {
+        if i > n { break; }
+        while n%i == 0 {
             ans.push(i);
-            m = m/i;
+            n /= i;
         }
     }
     ans
