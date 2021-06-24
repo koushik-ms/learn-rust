39f192230827b8f807b013084143872594b88482
koushik-ms <koushik.ms@gmail.com>
2021-06-24 23:35:57 +0000

    a functional unoin-find implementatoin

--- bs.io/p0150/src/lib.rs
+++ bs.io/p0150/src/lib.rs
@@ -1,4 +1,33 @@
-fn friend_groups(al: &Vec<Vec<i32>>) -> i32 { 2 }
+use std::collections::HashSet;
+
+fn root(ids: &mut Vec<usize>, mut i: usize) -> usize {
+    while i != ids[i] {
+        ids[i] = ids[ids[i]]; // path-compression
+        i = ids[i];
+    }
+    i
+}
+
+fn connect(ids: &mut Vec<usize>, from: usize, to:usize) {
+    let i = root(ids, from);
+    let j = root(ids, to);
+    ids[i] = j;
+}
+
+pub fn friend_groups(al: &Vec<Vec<usize>>) -> usize { 
+    let n = al.len();
+    let mut ids = (0..n).collect();
+    for (i,fl) in al.iter().enumerate() {
+        for &k in fl {
+            if root(&mut ids, i) != root(&mut ids, k) { connect(&mut ids, i, k); }
+        }
+    }
+    let mut fg = HashSet::new();
+    for i in 0..n {
+        fg.insert(root(&mut ids, i));
+    }
+    fg.len()
+}
 
 #[test]
 fn can_get_friend_groups() {
