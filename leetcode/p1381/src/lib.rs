638979599afafdbec09d007f07af71bbbb379cca
koushik-ms <koushik.ms@gmail.com>
2021-08-25 13:23:39 +0000

    add test for new, push and pop

--- leetcode/p1381/src/lib.rs
+++ leetcode/p1381/src/lib.rs
@@ -1,9 +1,24 @@
+struct CustomStack;
+#[allow(dead_code)]
+impl CustomStack {
+    fn new(_max_size: i32) -> Self { CustomStack{} }
+    fn push(&self, _value: i32) {}
+    fn pop(&self) -> i32 { 42 }
+}
 #[cfg(test)]
 mod tests {
+    use super::CustomStack;
     #[test]
     fn it_works() {
         assert_eq!(2 + 2, 4);
     }
+    #[test]
+    fn can_create_custom_stack() {
+        let x = 42;
+        let obj = CustomStack::new(1);
+        obj.push(x);
+        let ret_2: i32 = obj.pop();
+        assert_eq!(x, ret_2);
+    }
 }
 
-// https://leetcode.com/problems/design-a-stack-with-increment-operation/
