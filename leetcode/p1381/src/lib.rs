56f246b14aba9120e1085570457cc3ce988c91e2
koushik-ms <koushik.ms@gmail.com>
2021-08-28 06:36:06 +0000

    can increment all elements

--- leetcode/p1381/src/lib.rs
+++ leetcode/p1381/src/lib.rs
@@ -1,12 +1,14 @@
 struct CustomStack {
-    the: Vec<i32>
+    the: Vec<i32>,
+    inc: i32
 }
 
 #[allow(dead_code)]
 impl CustomStack {
-    fn new(_max_size: i32) -> Self { CustomStack{ the: vec![] } }
+    fn new(_max_size: i32) -> Self { CustomStack{ the: vec![], inc: 0 } }
     fn push(& mut self, value: i32) { self.the.push(value) }
-    fn pop(& mut self) -> i32 { self.the.pop().unwrap() }
+    fn pop(& mut self) -> i32 { self.the.pop().unwrap() + self.inc }
+    fn inc(& mut self, _extent: i32, value: i32) { self.inc = value; }
 }
 #[cfg(test)]
 mod tests {
@@ -35,6 +37,21 @@ mod tests {
             assert_eq!(*j, ret_2);
         }
     }
+    #[test]
+    fn can_apply_inc_to_all_items_pushed() {
+        let x = vec![2, 3, 4];
+        let size = 3;
+        let inc = 42;
+        let mut obj = CustomStack::new(size);
+        for i in x.iter() {
+            obj.push(*i);
+        }
+        obj.inc(size, inc);
+        for j in x.iter().rev(){
+            let ret_2: i32 = obj.pop();
+            assert_eq!(*j+inc, ret_2);
+        }
+    }
 }
 
 /*
@@ -46,6 +63,8 @@ push, apply inc to full stack, push more, pop
 push, apply inc twice (with diff extents)
 
 
+honor maxsize - push after passing maxsize will do nothing. 
+honor maxsize - inc for > maxsize elems will only act on maxsize elements.
 pop on empty stack returhs -1
 inc on empty stack has no effect 
 */
