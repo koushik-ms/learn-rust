7368d1531ca302c0efb6129784a5f28722ecc968
koushik-ms <koushik.ms@gmail.com>
2021-08-29 18:18:17 +0000

    implement new interface to pass test

--- bs.io/p0150/src/lib.rs
+++ bs.io/p0150/src/lib.rs
@@ -19,7 +19,9 @@ pub fn friend_groups(al: &Vec<Vec<usize>>) -> usize {
     let mut ids = (0..n).collect();
     for (i,fl) in al.iter().enumerate() {
         for &k in fl {
-            if root(&mut ids, i) != root(&mut ids, k) { connect(&mut ids, i, k); }
+            if root(&mut ids, i) != root(&mut ids, k) {
+                connect(&mut ids, i, k);
+            }
         }
     }
     let mut fg = HashSet::new();
@@ -30,35 +32,51 @@ pub fn friend_groups(al: &Vec<Vec<usize>>) -> usize {
 }
 
 struct Graph {
-    ids: Vec<usize>
+    ids: Vec<usize>,
 }
 
+impl Graph {
+    fn with_nodes(n: usize) -> Self {
+        Self {
+            ids: (0..n).collect(),
+        }
+    }
+    fn root(&mut self, mut i: usize) -> usize {
+        while i != self.ids[i] {
+            self.ids[i] = self.ids[self.ids[i]]; // path-compression
+            i = self.ids[i];
+        }
+        i
+    }
+    fn connect(&mut self, from: usize, to: usize) {
+        let i = self.root(from);
+        let j = self.root(to);
+        self.ids[i] = j;
+    }
+    fn roots(&mut self) -> HashSet<usize> {
+        let mut fg = HashSet::new();
+        (0..self.ids.len()).for_each(|x| {
+            fg.insert(self.root(x));
+        });
+        fg
+    }
+}
 
 #[test]
 fn can_get_friend_groups() {
-    let adjacency_list = vec![
-        vec![1, 2],
-        vec![],
-        vec![0],
-        vec![4],
-        vec![]
-    ];
+    let adjacency_list = vec![vec![1, 2], vec![], vec![0], vec![4], vec![]];
     assert_eq!(friend_groups(&adjacency_list), 2);
 }
 
 #[test]
 fn a_temp_test() {
-    let adjacency_list = vec![
-        vec![1, 2],
-        vec![],
-        vec![0],
-        vec![4],
-        vec![]
-    ];
-    let g = Graph::with_nodes(adjacency_list.len());
-    for (i,fl) in adjacency_list.iter().enumerate() {
+    let adjacency_list = vec![vec![1, 2], vec![], vec![0], vec![4], vec![]];
+    let mut g = Graph::with_nodes(adjacency_list.len());
+    for (i, fl) in adjacency_list.iter().enumerate() {
         for &k in fl {
-            if g.root(i) != g.root(k) { g.connect(i, k); }
+            if g.root(i) != g.root(k) {
+                g.connect(i, k);
+            }
         }
     }
     assert_eq!(g.roots().len(), 2);
