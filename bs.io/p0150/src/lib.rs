e531dc448f83885333bd60ee778b53f6a8182942
koushik-ms <koushik.ms@gmail.com>
2021-08-29 18:22:22 +0000

    remove old impementation and tests

--- bs.io/p0150/src/lib.rs
+++ bs.io/p0150/src/lib.rs
@@ -1,36 +1,5 @@
 use std::collections::HashSet;
 
-fn root(ids: &mut Vec<usize>, mut i: usize) -> usize {
-    while i != ids[i] {
-        ids[i] = ids[ids[i]]; // path-compression
-        i = ids[i];
-    }
-    i
-}
-
-fn connect(ids: &mut Vec<usize>, from: usize, to:usize) {
-    let i = root(ids, from);
-    let j = root(ids, to);
-    ids[i] = j;
-}
-
-pub fn fg2(al: &Vec<Vec<usize>>) -> usize {
-    let n = al.len();
-    let mut ids = (0..n).collect();
-    for (i, fl) in al.iter().enumerate() {
-        for &k in fl {
-            if root(&mut ids, i) != root(&mut ids, k) {
-                connect(&mut ids, i, k);
-            }
-        }
-    }
-    let mut fg = HashSet::new();
-    for i in 0..n {
-        fg.insert(root(&mut ids, i));
-    }
-    fg.len()
-}
-
 struct Graph {
     ids: Vec<usize>,
 }
@@ -76,12 +45,6 @@ pub fn friend_groups(adjacency_list: &Vec<Vec<usize>>) -> usize {
 
 #[test]
 fn can_get_friend_groups() {
-    let adjacency_list = vec![vec![1, 2], vec![], vec![0], vec![4], vec![]];
-    assert_eq!(friend_groups(&adjacency_list), 2);
-}
-
-#[test]
-fn a_temp_test() {
     let adjacency_list = vec![vec![1, 2], vec![], vec![0], vec![4], vec![]];
     assert_eq!(friend_groups(&adjacency_list), 2);
 }
