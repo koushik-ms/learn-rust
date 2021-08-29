6c6428d40c20cccba2d801727713e2c02af2b660
koushik-ms <koushik.ms@gmail.com>
2021-08-29 18:15:27 +0000

    rewite test using new interface (BROKEN)
    Introduce a new test that expresses the desired interface (the
    end-result of refactoring.
    
    This test, of course, fails.

--- bs.io/p0150/src/lib.rs
+++ bs.io/p0150/src/lib.rs
@@ -29,14 +29,37 @@ pub fn friend_groups(al: &Vec<Vec<usize>>) -> usize {
     fg.len()
 }
 
+struct Graph {
+    ids: Vec<usize>
+}
+
+
 #[test]
 fn can_get_friend_groups() {
-    let graph = vec![
+    let adjacency_list = vec![
         vec![1, 2],
         vec![],
         vec![0],
         vec![4],
         vec![]
     ];
-    assert_eq!(friend_groups(&graph), 2);
+    assert_eq!(friend_groups(&adjacency_list), 2);
+}
+
+#[test]
+fn a_temp_test() {
+    let adjacency_list = vec![
+        vec![1, 2],
+        vec![],
+        vec![0],
+        vec![4],
+        vec![]
+    ];
+    let g = Graph::with_nodes(adjacency_list.len());
+    for (i,fl) in adjacency_list.iter().enumerate() {
+        for &k in fl {
+            if g.root(i) != g.root(k) { g.connect(i, k); }
+        }
+    }
+    assert_eq!(g.roots().len(), 2);
 }
