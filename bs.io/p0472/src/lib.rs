d9bdc28ff17b53e6b621de9dfb7ce35a61d36cd8
koushik-ms <koushik.ms@gmail.com>
2021-09-01 18:20:03 +0000

    solve problem #472

--- bs.io/p0472/src/lib.rs
+++ bs.io/p0472/src/lib.rs
@@ -1,10 +1,25 @@
-pub fn increasing_digits(_n: u32) -> u32 { 36 }
+pub fn increasing_digits(n: u32) -> u32 {
+    let (mut num, mut den) = (1, 1);
+    for i in 0..n {
+        num *= 9-i;
+        den *= i+1;
+    }
+    num/den
+}
 
 #[cfg(test)]
 mod tests {
     use super::increasing_digits;
     #[test]
-    fn t_84_numbers_with_2_increasing_digits() {
+    fn t_036_numbers_with_2_increasing_digits() {
         assert_eq!(increasing_digits(2), 36);
+    }
+    #[test]
+    fn t_084_numbers_with_3_increasing_digits() {
+        assert_eq!(increasing_digits(3), 84);
+    }
+    #[test]
+    fn t_126_numbers_with_4_increasing_digits() {
+        assert_eq!(increasing_digits(4), 126);
     }
 }
