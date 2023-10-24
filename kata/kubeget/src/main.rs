bef81948db09b6bd8b62d6558cd3688daa512cbb
Meenakshisundaram Koushik <koushik.ms@gmail.com>
2023-10-24 10:47:26 +0530

    read and print data in configmap

--- kata/kubeget/src/main.rs
+++ kata/kubeget/src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
+// use k8s_openapi::api::core::v1::Pod;
+use k8s_openapi::api::core::v1::ConfigMap;
+use kube::{Client, Api};
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
     println!("Hello, world!");
+    let client = Client::try_default().await?;
+    let cm_api: Api<ConfigMap> = Api::namespaced(client, "kube-system");
+    let cm = cm_api.get("coredns").await?;
+    if let Some(data) = cm.data {
+        println!("Obtained Data: {:?}", data);
+    }
+    Ok(())
 }
